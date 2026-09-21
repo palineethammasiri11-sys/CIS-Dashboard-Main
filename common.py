8b8d8cc438d2c3d5f2e5452bbe65150541ae7e66
cefillll <boupisutbua2547@gmail.com>
2026-09-21 14:18:45 +0700

    Fix typo in sector mapping for ADVANC, TRUE, and THCOM

--- common.py
+++ common.py
@@ -32,7 +32,7 @@ from dataclasses import dataclass
 TARGET_STOCKS = ['ADVANC', 'CCET', 'DELTA', 'HANA', 'JMART', 'KCE', 'THCOM', 'TRUE']
 
 SECTOR_MAP = {
-    'ADVANC': 'Technology & Telecom', 'TRUE': 'Technology & Telecom', 'THCOM': 'Technology & Telecom',
+    'ADVANC': 'Technology & Telecomm', 'TRUE': 'Technology & Telecomm', 'THCOM': 'Technology & Telecomm',
     'DELTA': 'Electronic Components', 'HANA': 'Electronic Components', 'KCE': 'Electronic Components',
     'CCET': 'Electronic Components', 'JMART': 'Commerce & Technology'
 }
